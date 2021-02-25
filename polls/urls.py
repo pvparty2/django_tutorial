f7cbfe2ac63a5e5e5f9c85476686c5ffe1ead302
Partyka <O733503@NAEAST.AD.JPMORGANCHASE.com>
2021-02-25 11:32:09 -0600

    add notes

--- polls/urls.py
+++ polls/urls.py
@@ -4,3 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index')
 ]
+
+# path takes 4 arguments; 2 required and 2 optional
+#   route - string that contains URL pattern
+#   view
+#   kwargs
+#   name - lets you refer to URL unambiguously within templates
