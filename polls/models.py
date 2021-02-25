3249c6a5d2873e9352d95656bbbd141cb28a6330
Partyka <O733503@NAEAST.AD.JPMORGANCHASE.com>
2021-02-25 11:47:06 -0600

    create 2 models: Question and Choice. Create 2 fields in Question; create 2 fields and 1 relationship in Choice

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    # Each variable represents a field in a database.
+    question_text = models.CharField(max_length=200)
+    # all fields first optional argument is an alternative human-readable name
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
