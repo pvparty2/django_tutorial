59ebc95524b9d4e96292eef3d7e464e207343742
pvparty <fake@example.com>
2021-02-27 13:04:32 -0600

    create 3 new views and link them to urls

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('Hello world. You\'re at the polls index.')
+
+def detail(request, question_id):
+    return HttpResponse(f'You\'re looking at question {question_id}.')
+
+def results(request, question_id):
+    response = f'You\'re looking at results of question {question_id}.'
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    return HttpResponse(f'You\'re voting on question {question_id}.')
