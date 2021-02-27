233867dd156ab0ffb6f33d255bc3288d306f9a5a
pvparty <fake@example.com>
2021-02-27 13:23:10 -0600

    create index.html template and update index views function

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello world. You\'re at the polls index.')
+    # create list of first 5 questions ordered by decreasing publication date
+    latest_question_list = Question.objects.order_by('-pub_date')[:5] 
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse(f'You\'re looking at question {question_id}.')
