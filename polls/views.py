a85658a14bb120a61a4ac91d88c6e32739de96de
pvparty <fake@example.com>
2021-02-28 09:15:17 -0600

    before generic views

--- polls/views.py
+++ polls/views.py
@@ -15,12 +15,12 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    # Code below does the same as one liner above.
+    # Code below does the same as one liner.
     # try:
     #     question = Question.objects.get(pk=question_id)
     # except Question.DoesNotExist:
     #     raise Http404('Question does not exist.')
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
